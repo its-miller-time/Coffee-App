9dc76f6e7a7e82ece7461a561c9c09b9c058c826
Ian Miller <ian_c_miller@icloud.com>
2019-08-09 10:51:05 -0400

    first commit

---
+++ seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO coffee
+    (name,bespoke,size)
+VALUES 
+    ('Amy','Coffee Black', 'Grande'),
+    ('Andy','Americano Extra Cream Sugar', 'Venti'),
+    ('Arethra','Cappuccino Light Foam', 'Tall'),
+    ('Alex','Breve Latte Extra Sugar', 'Extra Grande')
